66edb0d591049d130632b8b8d12271e77b8124b7
selva-amano <amano@selva-i.co.jp>
2025-07-17 16:35:07 +0900

    seeder改造

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -11,30 +11,20 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
+    protected static int $userCount = 1; // 連番用の静的カウンタ
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
-            'email' => 'test' . rand(1, 30) . '@test.com',
+            'email' => 'test' . static::$userCount++ . '@test.com', // 連番メール
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
@@ -42,3 +32,4 @@ class UserFactory extends Factory
         ]);
     }
 }
+
