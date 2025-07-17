1a3099923d0e96042b70ea8ac93409938bee0acc
selva-amano <amano@selva-i.co.jp>
2025-07-17 16:41:06 +0900

    userseederいらんかった

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,6 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             PrefectureSeeder::class,
-            UserSeeder::class,
             TeamSeeder::class,
         ]);
     }
